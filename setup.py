6294a693e99f11cd2f809876138451be6a414c61
Dalton Serey <daltonserey@gmail.com>
2022-03-16 19:01:59 -0300

    fix: make setup.py upload README.md to pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 from pathlib import Path
 
-cwd = Path(__file__).parent
-long_description = (cwd / "README.md").read_text()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='codequery',
-    version='0.1.0',
+    version='0.1.2',
     description='Queries and code facts checking support for python source code',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/daltonserey/codequery',
     author='Dalton Serey',
     author_email='daltonserey@gmail.com',
     maintainer='Dalton Serey',
     maintainer_email='daltonserey@gmail.com',
     license='MIT',
-    long_description=long_description,
     py_modules=['codequery'],
     python_requires='>3.6',
-    install_requires=['pytest>=5.0.0'],
 )
