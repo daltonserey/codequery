f19768dcac962cc202c359495e45f7b8100cde16
Dalton Serey <daltonserey@gmail.com>
2022-07-16 15:32:35 -0300

    v0.1.4: bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='codequery',
-    version='0.1.3',
+    version='0.1.4',
     description='Queries and code facts checking support for python source code',
     long_description=long_description,
     long_description_content_type='text/markdown',
